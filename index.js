d09e5a5275bbb4831fe7cd682aa6cf47a842902d
NIcolasBonamy <n.bonamy@live.fr>
2021-01-14 12:08:13 +0100

    add categories requests

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const connection = require('./databases/database');
 
-const { SERVER_PORT, CLIENT_URL, JWT_SECRET } = process.env; // (attention!!!)
+const { SERVER_PORT, CLIENT_URL } = process.env; // (attention!!!)
 
 const app = express();
 
@@ -17,112 +15,77 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/* register */
+/* filter categories */
 
-app.post('/register', (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res
-      .status(400)
-      .json({ errorMessage: 'Please specify both email and password' });
-  } else {
-    const hash = bcrypt.hashSync(password, 10);
-    connection.query(
-      `INSERT INTO user(email, password) VALUES (?, ?)`,
-      [email, hash],
+app.get('/categories/inutile', (req, res) => {
+  connection.query(
+    `SELECT * FROM super_pouvoir sp JOIN categorie cat ON sp.categorie_idcategorie = cat.idcategorie WHERE cat.name = "inutile"`,
       (error, result) => {
         if (error) {
           res.status(500).json({ errorMessage: error.message });
         } else {
-          res.status(201).json({
-            id: result.insertId,
-            email,
-            password: 'hidden',
-          });
+          res.status(201).json({...result});
         }
       }
     );
   }
-});
+);
 
-/* login */
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res
-      .status(400)
-      .json({ errorMessage: 'Please specify both email and password' });
-  } else {
-    connection.query(
-      `SELECT * FROM user WHERE email=?`,
-      [email],
+app.get('/categories/environnemental', (req, res) => {
+  connection.query(
+    `SELECT * FROM super_pouvoir sp JOIN categorie cat ON sp.categorie_idcategorie = cat.idcategorie WHERE cat.name = "environnemental"`,
       (error, result) => {
         if (error) {
           res.status(500).json({ errorMessage: error.message });
-        } else if (result.length === 0) {
-          res.status(403).json({ errorMessage: 'Invalid email' });
-        } else if (bcrypt.compareSync(password, result[0].password)) {
-          // Passwords match (attention!!!)
-          const user = {
-            id: result[0].id,
-            email,
-            password: 'hidden',
-          };
-          const token = jwt.sign({ id: user.id }, JWT_SECRET, {
-            expiresIn: '1h',
-          });
-          res.status(200).json({ user, token });
         } else {
-          // Passwords don't match (attention!!!)
-          res.status(403).json({ errorMessage: 'Invalid password' });
+          res.status(201).json({...result});
         }
       }
     );
   }
-});
+);
 
-/* Afficher la liste des utilisateurs */
-
-// Token middleware (attention!!!)
-const authenticateWithJsonWebToken = (req, res, next) => {
-  if (req.headers.authorization !== undefined) {
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, JWT_SECRET, (err) => {
-      if (err) {
-        res
-          .status(401)
-          .json({ errorMessage: "you're not allowed to access these data" });
-      } else {
-        next();
+app.get('/categories/corporel', (req, res) => {
+  connection.query(
+    `SELECT * FROM super_pouvoir sp JOIN categorie cat ON sp.categorie_idcategorie = cat.idcategorie WHERE cat.name = "corporel"`,
+      (error, result) => {
+        if (error) {
+          res.status(500).json({ errorMessage: error.message });
+        } else {
+          res.status(201).json({...result});
+        }
       }
-    });
-  } else {
-    res
-      .status(401)
-      .json({ errorMessage: "you're not allowed to access these data" });
+    );
   }
-};
+);
 
-// Authenticated route
-app.get('/users', authenticateWithJsonWebToken, (req, res) => {
-  connection.query(`SELECT * FROM user`, (error, result) => {
-    if (error) {
-      res.status(500).json({ errorMessage: error.message });
-    } else {
-      res.status(200).json(
-        result.map((user) => {
-          return { ...user, password: 'hidden' };
-        })
-      );
-    }
-  });
-});
+app.get('/categories/classique', (req, res) => {
+  connection.query(
+    `SELECT * FROM super_pouvoir sp JOIN categorie cat ON sp.categorie_idcategorie = cat.idcategorie WHERE cat.name = "classique"`,
+      (error, result) => {
+        if (error) {
+          res.status(500).json({ errorMessage: error.message });
+        } else {
+          res.status(201).json({...result});
+        }
+      }
+    );
+  }
+);
 
-
-
-
-
+app.get('/categories/flippant', (req, res) => {
+  connection.query(
+    `SELECT * FROM super_pouvoir sp JOIN categorie cat ON sp.categorie_idcategorie = cat.idcategorie WHERE cat.name = "flippant"`,
+      (error, result) => {
+        if (error) {
+          res.status(500).json({ errorMessage: error.message });
+        } else {
+          res.status(201).json({...result});
+        }
+      }
+    );
+  }
+);
 
 // Don't write anything below this line!
 app.listen(SERVER_PORT, () => {
